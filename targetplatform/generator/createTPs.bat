e924313d26e4cf26bfd25fc66634e9298acbec4a
Marco Grawunder <Marco.Grawunder@uni-oldenburg.de>
2019-09-24 11:20:12 +0200

    Create TPs by script (currently only windows)

---
+++ targetplatform/generator/createTPs.bat
@@ -0,0 +1,31 @@
+REM Simple windows script to create the required target platforms
+
+del generated\platform_core.target
+type base\tp_header.xml base\core_locations.xml base\ody_resources_location.xml base\tp_footer.xml > tmp.target
+powershell -Command "(gc tmp.target) -replace '__TITLE__', '(Core)' | Out-File -encoding ASCII generated\platform_core.target"
+
+del generated\platform_master_stable.target 
+type base\tp_header.xml base\core_locations.xml base\ody_resources_location.xml base\ody_stable_locations.xml base\tp_footer.xml > tmp.target
+powershell -Command "(gc tmp.target) -replace '__TITLE__', '(master stable)' | Out-File -encoding ASCII tmp.target"
+powershell -Command "(gc tmp.target) -replace '__BRANCH__', 'master' | Out-File -encoding ASCII generated\platform_master_stable.target"
+del tmp.target
+
+del generated\platform_development_stable.target
+type base\tp_header.xml base\core_locations.xml base\ody_resources_location.xml base\ody_stable_locations.xml base\tp_footer.xml > tmp.target
+powershell -Command "(gc tmp.target) -replace '__TITLE__', '(development stable)' | Out-File -encoding ASCII tmp.target"
+powershell -Command "(gc tmp.target) -replace '__BRANCH__', 'development'| Out-File -encoding ASCII generated\platform_development_stable.target"
+del tmp.target
+
+del generated\platform_master_all.target
+type base\tp_header.xml base\core_locations.xml base\ody_resources_location.xml base\ody_stable_locations.xml base\ody_incubation_locations.xml base\tp_footer.xml > tmp.target
+powershell -Command "(gc tmp.target) -replace '__TITLE__', '(master all)' | Out-File -encoding ASCII tmp.target"
+powershell -Command "(gc tmp.target) -replace '__BRANCH__', 'master' | Out-File -encoding ASCII generated\platform_master_all.target"
+del tmp.target
+
+del generated\platform_development_all.target
+type base\tp_header.xml base\core_locations.xml base\ody_resources_location.xml base\ody_stable_locations.xml base\ody_incubation_locations.xml base\tp_footer.xml > tmp.target
+powershell -Command "(gc tmp.target) -replace '__TITLE__', '(development all)' | Out-File -encoding ASCII tmp.target"
+powershell -Command "(gc tmp.target) -replace '__BRANCH__', 'development' | Out-File -encoding ASCII generated\platform_development_all.target"
+del tmp.target
+
+
