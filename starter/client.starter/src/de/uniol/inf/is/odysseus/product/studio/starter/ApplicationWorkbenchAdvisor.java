410618babec47b1aec91c9691609f6c339e2f545
Marco Grawunder <marco.grawunder@uni-oldenburg.de>
2020-07-03 08:37:21 +0200

    ODY-1985: Added images for explorer, now working again

--- starter/client.starter/src/de/uniol/inf/is/odysseus/product/studio/starter/ApplicationWorkbenchAdvisor.java
+++ starter/client.starter/src/de/uniol/inf/is/odysseus/product/studio/starter/ApplicationWorkbenchAdvisor.java
@@ -15,11 +15,18 @@
  */
 package de.uniol.inf.is.odysseus.product.studio.starter;
 
+import java.net.URL;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
+import org.osgi.framework.Bundle;
+
 
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
@@ -40,8 +47,23 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
 		IDE.registerAdapters();
 		
+		// inserted: register images for rendering explorer view
+	    final String ICONS_PATH = "icons/full/";
+	    final String PATH_OBJECT = ICONS_PATH + "obj16/";
+	    Bundle ideBundle = Platform.getBundle(IDEWorkbenchPlugin.IDE_WORKBENCH);
+	    declareWorkbenchImage(configurer, ideBundle,
+	        IDE.SharedImages.IMG_OBJ_PROJECT, PATH_OBJECT + "prj_obj.gif", true);
+	    declareWorkbenchImage(configurer, ideBundle,
+	        IDE.SharedImages.IMG_OBJ_PROJECT_CLOSED, PATH_OBJECT + "cprj_obj.gif", true);
 
 	   BrokenIDEImagesLoader.redeclareImages(configurer);
+	}
+	
+	private void declareWorkbenchImage(IWorkbenchConfigurer configurer_p,
+	        Bundle ideBundle, String symbolicName, String path, boolean shared) {
+	    URL url = ideBundle.getEntry(path);
+	    ImageDescriptor desc = ImageDescriptor.createFromURL(url);
+	    configurer_p.declareImage(symbolicName, desc, shared);
 	}
 
 	@Override
